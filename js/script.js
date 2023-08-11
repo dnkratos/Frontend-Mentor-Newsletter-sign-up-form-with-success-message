d6be543aef166c51de51c52fe0f0bbdf99d443e2
dnkratos <duncarobertgabriel1994@gmail.com>
2023-08-11 17:57:58 +0300

    changed the functionality of the succes message from markup added with JS to a html dialog element

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 const submitBtn = document.querySelector("form button");
 const emailInput = document.querySelector("#email");
 const main = document.querySelector("main");
-const succesState = document.querySelector(".succes");
+
+const dialogElement = document.querySelector("dialog");
+const dismissBtn = document.querySelector("dialog.succes button");
 
 const validateEmail = function (inputText) {
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -18,37 +20,23 @@ const validateEmail = function (inputText) {
 submitBtn.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const markup = `<div>
-                        <figure>
-                             <img src="assets//images/icon-success.svg" alt="" />
-                        </figure>
-                        <h2>Thanks for subscribing!</h2>
-                        <p>A confirmation email has been sent to ${emailInput.value}. Please open it and click the button inside to confirm your subscription.</p>
-                    </div>
-                        <button>Dismiss message</button>`;
-
     if (emailInput.value === "" || emailInput.value === null) {
         emailInput.focus();
     }
 
     if (validateEmail(emailInput)) {
         main.classList.add("hidden");
-
-        succesState.classList.remove("hidden");
-        succesState.insertAdjacentHTML("afterbegin", markup);
+        dialogElement.showModal();
 
         emailInput.value = "";
-
-        const dismissBtn = document.querySelector(".succes button");
-        dismissBtn.addEventListener("click", function () {
-            succesState.classList.add("hidden");
-            main.classList.remove("hidden");
-
-            succesState.innerHTML = "";
-        });
     } else {
         emailInput.value = "";
         emailInput.focus();
         console.log("invalid email");
     }
+});
+
+dismissBtn.addEventListener("click", function () {
+    dialogElement.close();
+    main.classList.remove("hidden");
 });
